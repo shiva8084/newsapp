c2cc20af86d5e7b0ea5172670e629e8ad8866eb3
shiva8084 <shivamanand37447@gmail.com>
2024-11-06 22:06:08 +0530

    final COmmit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,129 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
+
+  state={
+    progress :0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <div>
-        <Navbar />
-        <News />
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+          />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="general"
+                  pageSize={5}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="business"
+                  pageSize={5}
+                  country="us"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="entertainment"
+                  pageSize={5}
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/general"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="general"
+                  pageSize={5}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="health"
+                  pageSize={5}
+                  country="us"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="science"
+                  pageSize={5}
+                  country="us"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="sports"
+                  pageSize={5}
+                  country="us"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News setProgress={this.setProgress} 
+                  key="technology"
+                  pageSize={5}
+                  country="us"
+                  category="technology"
+                />
+              }
+            />
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,38 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description,imageUrl,newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={!imageUrl?"https://images.axios.com/2CDAqkJm8gzAkUd2PLxvgwCSg7U=/0x0:1920x1080/1366x768/2024/10/31/1730402034081.jpg":imageUrl} className="card-img-top" alt="..." />
+        <div className="card">
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://images.axios.com/2CDAqkJm8gzAkUd2PLxvgwCSg7U=/0x0:1920x1080/1366x768/2024/10/31/1730402034081.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">
+              {title}
+              <span
+                className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+                style={{ left: "90%", zIndex: "1" }}
+              >
+                {source}
+              </span>
+            </h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} className="btn btn-sm btn-primary">
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a href={newsUrl} className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
